721075e1a150c9b89d5bd5d8c6dbe17414181ee4
fengzee <fengzee@fengzee.me>
2025-03-12 09:51:25 +0800

    Revert "响应式布局"
    This reverts commit ec30c2b742cb0785d4b226c381360d24ba0dd982.

--- packages/agamer-gui/src/main.js
+++ packages/agamer-gui/src/main.js
@@ -5,6 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 850,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
